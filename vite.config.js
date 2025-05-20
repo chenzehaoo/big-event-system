44a2d26a76cbba281764a33a885a3159a3c21c92
chenzehaoo <3585881508@qq.com>
2025-05-20 09:55:45 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 引入@vitejs/plugin-legacy
+// import legacy from '@vitejs/plugin-legacy'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    // legacy({
+    //   targets: ['defaults', 'not IE 11']
+    // }),
+
+    vue(),
+    vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
